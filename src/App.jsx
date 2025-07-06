ef0b48292c9207878d3edd79fa8e9f9ae0f64bb8
Thays Peixoto <thabysilva12@gmail.com>
2025-07-06 15:30:25 -0300

    ajuste de import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './Header'
+import Header from './components/Header';
+
 
 
 createRoot(document.getElementById('root')).render(
