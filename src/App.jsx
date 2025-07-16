69294d90b27b0094986961f0ffb0e8b3b9521c48
Thays Peixoto <thabysilva12@gmail.com>
2025-07-16 19:14:59 -0300

    Alterações realizadas para ajuste de layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header';
+import Nav from './components/Nav'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Nav />
   </StrictMode>,
 )
+
