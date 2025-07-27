baa3b48c862595d4c831ce53591c809d5e1751ab
Thays Peixoto <thabysilva12@gmail.com>
2025-07-26 22:14:30 -0300

    atualizar dependências do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import Header from './components/Header';
 import Nav from './components/Nav'
 import HeroSection from './components/HeroSection';
+import BeneSelection from './components/BeneSelection';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Nav />
      <HeroSection />
+     <BeneSelection />
   </StrictMode>,
 )
 
