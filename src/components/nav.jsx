d5248347cd6470f1b6bf626fc35df3c3a10fd0cd
Thays Peixoto <thabysilva12@gmail.com>
2026-04-14 14:51:27 -0300

    feat: adiciona mock para renderizacao do componente nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import "./Nav.css";
+import './Nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
+const navLinks = [
+  { label: 'Home', href: '#' },
+  { label: 'Shop', href: '#' },
+  { label: 'Pages', href: '#' },
+  { label: 'Blog', href: '#' },
+  { label: 'About Us', href: '#' },
+  { label: 'Contact Us', href: '#' },
+];
 
-
-
-function Nav(){
-
- return(
-
-  <nav className="nav-container">
-    <ul className="nav-list">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Shop</a></li>
-    <li><a href="#">Pages</a></li>
-    <li><a href="#">Blog</a></li>
-    <li><a href="#">About Us</a></li>
-    <li><a href="#">Contact Us</a></li>
-    <li className="telefone">
-    <FontAwesomeIcon icon={faPhone} className="icon-search" />
-    <span>(219)555 -0114</span>
-    </li>
-    </ul>
-    
-  </nav>
- )
-
+function Nav() {
+  return (
+    <nav className="nav-container" aria-label="Navegação principal">
+      <ul className="nav-list">
+        {navLinks.map((link) => (
+          <li key={link.label}>
+            <a href={link.href}>{link.label}</a>
+          </li>
+        ))}
+        <li className="telefone">
+          <FontAwesomeIcon icon={faPhone} className="icon-phone" />
+          <span>(219) 555-0114</span>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
