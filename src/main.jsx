ddde9626136df98ac8fff9bc0804e609e3056d22
Thays Peixoto <thabysilva12@gmail.com>
2026-04-14 17:10:03 -0300

    feat: recebe useContext para compartilhamento de dados e estados entre componentes

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { CartProvider } from './contexts/CartContext.jsx';
 import { FavoritesProvider } from './contexts/FavoritesContext.jsx';
+import { SearchProvider } from './contexts/SearchContext.jsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
       <FavoritesProvider>
-        <App />
+        <SearchProvider>
+          <App />
+        </SearchProvider>
       </FavoritesProvider>
     </CartProvider>
   </React.StrictMode>,
