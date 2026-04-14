5025ddeeba4a90a0ae8b43735f310a5fc245ea6c
Thays Peixoto <thabysilva12@gmail.com>
2026-04-14 14:38:56 -0300

    refactor: correções críticas na estrutura do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
+import { CartProvider } from './contexts/CartContext.jsx';
+import { FavoritesProvider } from './contexts/FavoritesContext.jsx';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </CartProvider>
   </React.StrictMode>,
 );
