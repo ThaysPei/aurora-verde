dea22e030bb5f97961044857a09e2d36741320fe
Thays Peixoto <thabysilva12@gmail.com>
2026-04-14 14:50:25 -0300

    refactor: ajuste de lógica e chamada do elemento na tag img

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,39 +1,40 @@
-import React from "react";
-import "./HeroSection.css";
-import heroImg from "../assets/hero-image.png"; // imagem principal
-import heroImg1 from "../assets/hero-image2.png";
-import heroImg2 from "../assets/hero-image3.png";
+import './HeroSection.css';
+import heroImg from '../assets/hero-image.png';
+import heroImg1 from '../assets/hero-image2.png';
+import heroImg2 from '../assets/hero-image3.png';
 
-
-
-const HeroSection = () => {
+function HeroSection() {
   return (
-    <section className="hero-section">
+    <section className="hero-section" aria-label="Banner principal">
       <div className="hero-wrapper">
-        
-        {/* Imagem principal com botão */}
         <div className="hero-image-container">
-            <a href="#">
-          <img src={heroImg} alt="Promoção orgânicos" className="hero-image" />
-          </a>
-        </div>
-        
-        {/* Segunda imagem com botão */}
-        <div className="hero-image-container2">
-            <a href="#">
-          <img src={heroImg1} alt="Promoção orgânicos" className="hero-image2" />
+          <a href="#" aria-label="Ver promoção de orgânicos">
+            <img src={heroImg} alt="Promoção orgânicos" className="hero-image" />
           </a>
         </div>
 
-       <div className="hero-image-container3">
-       <a href="#">
-       <img src={heroImg2} alt="Promoção orgânicos" className="hero-image3" />
-        </a>
+        <div className="hero-image-container">
+          <a href="#" aria-label="Ver segunda promoção">
+            <img
+              src={heroImg1}
+              alt="Segunda promoção"
+              className="hero-image-small"
+            />
+          </a>
         </div>
-    </div> 
+
+        <div className="hero-image-container">
+          <a href="#" aria-label="Ver terceira promoção">
+            <img
+              src={heroImg2}
+              alt="Terceira promoção"
+              className="hero-image-small"
+            />
+          </a>
+        </div>
+      </div>
     </section>
   );
-};
+}
 
 export default HeroSection;
-
